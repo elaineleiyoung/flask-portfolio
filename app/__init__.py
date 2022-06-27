31f0872ee0bed2647c4f4f08ceb9d0a0100802ec
Elaine Leiyoung <92842749+elaineleiyoung@users.noreply.github.com>
2022-06-26 21:27:00 -0700

    Implemented timeline posts

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,31 @@
 import os
 from flask import Flask, render_template, request, abort
 from dotenv import load_dotenv
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta: 
+            database = mydb
+mydb.connect()
+mydb.create_tables([TimelinePost])
+print(mydb)
 
 STAR_USER_INFO = {
     'hobbies':{
@@ -52,10 +74,6 @@ ELAINE_USER_INFO = {
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
 @app.route('/education')
 def education():
     return render_template('education.html', user = STAR_USER_INFO)
@@ -72,6 +90,35 @@ def about():
 def hobbies():
     return render_template('hobbies.html', user = ELAINE_USER_INFO)
 
-@app.route('/countries')
-def countries():
-    return render_template('countries.html')
+@app.route('/timeline')
+def timeline():
+    posts = [model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())]
+    return render_template('timeline.html', title = "Timeline", url=os.getenv("URL"), posts=posts)
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+   return {
+       'timeline_posts': [
+           model_to_dict(p)
+           for p in
+TimelinePost.select().order_by(TimelinePost.created_at.desc())
+]
+}
+@app.route("/api/timeline_post/<id>", methods=["DELETE"])
+def timeline_delete(id):
+    guide = TimelinePost.query.get(id)
+    mydb.session.delete(guide)
+    mydb.session.commit()
+
+    return "Guide was successfully deleted"
+
+
